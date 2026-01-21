c74c0ea6f44cc44b28ed2b470408d8efc64abcee
Sampadabandre <sampadabandre2@gmail.com>
2026-01-21 10:45:29 +0530

    Add files via upload

---
+++ exp_2_switch_case.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+int main()
+{
+int day=3;
+switch (day) {
+case1: printf("M");
+break;
+case2: printf("Tue");
+break;
+case3: printf("Wed");
+break;
+case4: printf("thu");
+break;
+case5: printf("fri");
+break;
+case6: printf("sat");
+break;
+case7:printf("sun");
+
+
+break;
+default: printf("Invalid day");
+}
+return 0;
+}
